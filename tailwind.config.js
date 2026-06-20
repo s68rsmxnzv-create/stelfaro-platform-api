80d655a5c6aa8dff6a36da674a2791521683ec46
Jose Armando <jarmandocramirez@gmail.com>
2026-06-20 23:16:15 +0200

    Usa dark mode por clase en Inertia

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ import { stelfaroTheme } from '../stelfaro-platform/packages/ui/tailwind-theme.j
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
